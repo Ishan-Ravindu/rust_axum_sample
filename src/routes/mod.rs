80bf51300230414a95dc8ab25cc7a0db09a262f1
Ishan Ravindu <ishanravindu27596@gmail.com>
2023-04-16 13:00:41 +0530

    middleware

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,32 +1,61 @@
 mod hello_world;
-mod mirror_body_string;
+mod middleware_message;
 mod mirror_body_json;
+mod mirror_body_string;
+mod mirror_custom_header;
+mod mirror_user_agent;
 mod path_variables;
 mod query_params;
-mod mirror_user_agent;
-mod mirror_custom_header;
+mod read_middleware_custom_header;
+mod set_middleware_custom_header;
 
 use axum::{
     body::Body,
+    http::Method,
     routing::{get, post},
-    Router, 
+    Extension, Router, middleware,
 };
 use hello_world::hello_world;
-use mirror_body_string::mirror_body_string;
+use middleware_message::middleware_message;
 use mirror_body_json::mirror_body_json;
-use path_variables::{path_variables,hard_coded_path};
-use query_params::query_params;
-use mirror_user_agent::mirror_user_agent;
+use mirror_body_string::mirror_body_string;
 use mirror_custom_header::mirror_custom_header;
+use mirror_user_agent::mirror_user_agent;
+use path_variables::{hard_coded_path, path_variables};
+use query_params::query_params;
+use read_middleware_custom_header::read_middleware_custom_header;
+use set_middleware_custom_header::set_middleware_custom_header;
+use tower_http::cors::{Any, CorsLayer};
 
-pub fn create_routes()->Router<Body>{
+#[derive(Clone)]
+pub struct SharedData {
+    message: String,
+}
+
+pub fn create_routes() -> Router<Body> {
+    let cors = CorsLayer::new()
+        .allow_methods([Method::POST, Method::GET])
+        .allow_origin(Any);
+
+    let shared_data = SharedData {
+        message: "hello from shared data".to_owned(),
+    };
+
     Router::new()
+        .route(
+            "/read_middleware_custom_header",
+            get(read_middleware_custom_header),
+        )
+        .route_layer(middleware::from_fn(set_middleware_custom_header))
         .route("/", get(hello_world))
         .route("/mirror_body_string", post(mirror_body_string))
         .route("/mirror_body_json", post(mirror_body_json))
         .route("/path_variables/50", get(hard_coded_path))
         .route("/path_variables/:id", get(path_variables))
-        .route("/query_params",get(query_params))
-        .route("/mirror_user_agent",get(mirror_user_agent))
-        .route("/mirror_custom_header",get(mirror_custom_header))
-} 
+        .route("/query_params", get(query_params))
+        .route("/mirror_user_agent", get(mirror_user_agent))
+        .route("/mirror_custom_header", get(mirror_custom_header))
+        .route("/middleware_message", get(middleware_message))
+        .layer(Extension(shared_data))
+        .layer(cors)
+}
