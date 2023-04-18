caca3bb68983ac64f438b2ab7388e2bf9834701b
Ishan Ravindu <ishanravindu27596@gmail.com>
2023-04-18 16:01:30 +0530

    setup database

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,14 @@
 use routes::create_routes;
+use sea_orm::{DatabaseConnection,Database};
 
 mod routes;
 
-pub async fn run(){
+pub async fn run(database_uri:&str){
+
+    let db: DatabaseConnection = Database::connect(database_uri)
+        .await
+        .expect("Database connection failed");
+
     let app = create_routes();
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
+use dotenvy::dotenv;
+use std::env;
 use rust_axum_sample::run;
 
+
 #[tokio::main]
-async fn main() {
-   run().await;
+async fn main() { 
+   
+   dotenv().ok();
+   let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+
+   run(&db_url).await;
 }
 
